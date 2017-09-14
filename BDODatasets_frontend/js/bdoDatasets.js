e098e49fb4333b9e06c0b7c6d59fe611e53186b5
Jaime M Trillos <jtrillos@users.noreply.github.com>
2017-09-14 14:18:26 +0200

    Update bdoDatasets.js

--- BDODatasets_frontend/js/bdoDatasets.js
+++ BDODatasets_frontend/js/bdoDatasets.js
@@ -8,20 +8,23 @@ function cancelButton() {
 }
 
 function submitUriButton(form) {
-    var x = form.uri.value;
-    var uri = encodeURI(x);
+    var uri = encodeURI(form.uri.value);
     console.log("this is " + uri);
-    var command = "/home/anatrillos/Documents/BigDataOcean-Harmonization/Backend/bdodatasets/target/BDODatsets-bdodatasets/BDODatsets/bin/suggest \""+ uri +"\"";
-    var exec = require('child_process').exec();
-    child = exec(command,
-        function (error, stdout, stderr) {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error !== null) {
-                console.log('exec error: ' + error);
+    if (!isURL(uri)){
+        alert("ERROR");
+    }else {
+        var command = "/home/anatrillos/Documents/BigDataOcean-Harmonization/Backend/bdodatasets/target/BDODatsets-bdodatasets/BDODatsets/bin/suggest \""+ uri +"\"";
+        var exec = require('child_process').exec();
+        child = exec(command,
+            function (error, stdout, stderr) {
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
             }
-        }
-    )
+        )
+    }
 }
 
 function isNumberKey(evt) {
@@ -29,4 +32,14 @@ function isNumberKey(evt) {
     if (charCode > 31 && (charCode < 48 || charCode > 57))
         return false;
     return true;
+}
+
+function isURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
 }
