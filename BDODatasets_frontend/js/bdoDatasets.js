1d61444d2b60be8e3a97ab81699e407537a45ff9
Jaime M Trillos <jtrillos@users.noreply.github.com>
2017-09-14 11:34:38 +0200

    Update bdoDatasets.js

--- BDODatasets_frontend/js/bdoDatasets.js
+++ BDODatasets_frontend/js/bdoDatasets.js
@@ -8,25 +8,20 @@ function cancelButton() {
 }
 
 function submitUriButton(form) {
-    var uri = form.uri.value;
-    var x = encodeURI(uri);
-
-    if (ValidURL(x)){
-        alert("dddd", x);
-       /* var command = "/home/anatrillos/Documents/BigDataOcean-Harmonization/Backend/bdodatasets/target/BDODatsets-bdodatasets/BDODatsets/bin/suggest" + uri;
-        var exec = require('child_process').exec();
-        child = exec(command,
-            function (error, stdout, stderr) {
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }
+    var x = form.uri.value;
+    var uri = encodeURI(x);
+    console.log("this is " + uri);
+    var command = "/home/anatrillos/Documents/BigDataOcean-Harmonization/Backend/bdodatasets/target/BDODatsets-bdodatasets/BDODatsets/bin/suggest \""+ uri +"\"";
+    var exec = require('child_process').exec();
+    child = exec(command,
+        function (error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
             }
-        )*/
-    }else {
-        console.log('crap');
-    }
+        }
+    )
 }
 
 function isNumberKey(evt) {
@@ -34,19 +29,4 @@ function isNumberKey(evt) {
     if (charCode > 31 && (charCode < 48 || charCode > 57))
         return false;
     return true;
-}
-
-function ValidURL(str) {
-    var pattern = new RegExp('^(https?:\/\/)?' + // protocol
-        '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|' + // domain name
-        '((\d{1,3}\.){3}\d{1,3}))' + // OR ip (v4) address
-        '(\:\d+)?(\/[-a-z\d%_.~+]*)*' + // port and path
-        '(\?[;&a-z\d%_.~+=-]*)?' + // query string
-        '(\#[-a-z\d_]*)?$', 'i'); // fragment locater
-    if (!pattern.test(str)) {
-        alert("Please enter a valid URL.");
-        return false;
-    } else {
-        return true;
-    }
 }
