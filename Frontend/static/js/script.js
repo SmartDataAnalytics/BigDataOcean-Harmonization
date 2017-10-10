fe3caaa9d5f2944c61e4d3b4f334c5831a57f61e
Jaime M Trillos <jaimemanueltrillos@gmail.com>
2017-10-10 12:04:14 +0200

    new changes

--- Frontend/static/js/script.js
+++ Frontend/static/js/script.js
@@ -8,98 +8,35 @@ $(document).on("click", ".add-button", function(e) {
 	})
 });
 
-var citynames = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: {
-    url: '/json/resourceType.json',
-    filter: function(list) {
-      return $.map(list, function(type) {
-        return { name: type }; });
-    }
-  }
-});
-citynames.initialize();
-
-/*var cities = new Bloodhound({
+var resourceTypes = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: 'assets/cities.json'
+  prefetch: 'static/json/resourceType.json'
 });
-cities.initialize();*/
+resourceTypes.initialize();
 
-/**
- * Typeahead
- */
-var elt = $('.example_typeahead > > input');
-elt.tagsinput({
-  typeaheadjs: {
-    name: 'citynames',
-    displayKey: 'name',
-    valueKey: 'name',
-    source: citynames.ttAdapter()
-  }
-});
-
-/**
- * Objects as tags
- */
-elt = $('.example_objects_as_tags > > input');
+var elt = $('#Type');
 elt.tagsinput({
   itemValue: 'value',
   itemText: 'text',
   typeaheadjs: {
-    name: 'cities',
+    name: 'resourceType',
     displayKey: 'text',
-    source: cities.ttAdapter()
+    source: resourceTypes.ttAdapter()
   }
 });
 
-elt.tagsinput('add', { "value": 1 , "text": "Amsterdam"   , "continent": "Europe"    });
-elt.tagsinput('add', { "value": 4 , "text": "Washington"  , "continent": "America"   });
-elt.tagsinput('add', { "value": 7 , "text": "Sydney"      , "continent": "Australia" });
-elt.tagsinput('add', { "value": 10, "text": "Beijing"     , "continent": "Asia"      });
-elt.tagsinput('add', { "value": 13, "text": "Cairo"       , "continent": "Africa"    });
+elt.tagsinput('add', { "value": "http://inspire.ec.europa.eu/metadata-codelist/ResourceType/series" , "text": "Series" });
+elt.tagsinput('add', { "value": "http://inspire.ec.europa.eu/metadata-codelist/ResourceType/dataset" , "text": "Dataset"});
 
-/**
- * Categorizing tags
- */
-/*elt = $('.example_tagclass > > input');
-elt.tagsinput({
-  tagClass: function(item) {
-    switch (item.continent) {
-      case 'Europe'   : return 'label label-primary';
-      case 'America'  : return 'label label-danger label-important';
-      case 'Australia': return 'label label-success';
-      case 'Africa'   : return 'label label-default';
-      case 'Asia'     : return 'label label-warning';
-    }
-  },
-  itemValue: 'value',
-  itemText: 'text',
-  // typeaheadjs: {
-  //   name: 'cities',
-  //   displayKey: 'text',
-  //   source: cities.ttAdapter()
-  // }
-  typeaheadjs: [
-  {
-      hint: true,
-     highlight: true,
-     minLength: 2
-  },
-   {
-      name: 'cities',
-       displayKey: 'text',
-       source: cities.ttAdapter()
-   }
-  ]
-});
-elt.tagsinput('add', { "value": 1 , "text": "Amsterdam"   , "continent": "Europe"    });
-elt.tagsinput('add', { "value": 4 , "text": "Washington"  , "continent": "America"   });
-elt.tagsinput('add', { "value": 7 , "text": "Sydney"      , "continent": "Australia" });
-elt.tagsinput('add', { "value": 10, "text": "Beijing"     , "continent": "Asia"      });
-elt.tagsinput('add', { "value": 13, "text": "Cairo"       , "continent": "Africa"    });
-*/
 // HACK: overrule hardcoded display inline-block of typeahead.js
 $(".twitter-typeahead").css('display', 'inline');
+
+
+$('.bootstrap-tagsinput > > input').tokenfield({
+  autocomplete: {
+    source: ['red','blue','green','yellow','violet','brown','purple','black','white'],
+    delay: 100
+  },
+  showAutocompleteOnFocus: true
+})
