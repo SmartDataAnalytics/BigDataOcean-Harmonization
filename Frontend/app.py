b428b2d1013db368ca7b569703302fe12ae23fdb
Ana Trillos <anatrillosu@gmail.com>
2017-11-10 08:46:59 +0100

    minor changes

--- Frontend/app.py
+++ Frontend/app.py
@@ -8,8 +8,8 @@ import os
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-globalPath = "/home/jaimetrillos/Dropbox/BDO/BigDataOcean-Harmonization"
-#globalPath = "/home/anatrillos/Dropbox/Documentos/BigDataOcean-Harmonization"
+# globalPath = "/home/jaimetrillos/Dropbox/BDO/BigDataOcean-Harmonization"
+globalPath = "/home/anatrillos/Dropbox/Documentos/BigDataOcean-Harmonization"
 
 data = [{
   "title": "Hi",
@@ -50,57 +50,61 @@ def parse():
 		else :
 			return render_template('addMetadata.html', dataset="")
 
-@app.route('/save', methods=['POST'])
+@app.route('/save', methods=['GET','POST'])
 def save():
 	if request.method == 'POST':
-		uri = "<http://bigdataocean.eu/bdo/"+request.form['identifier']+">"
-		with open(globalPath + '/Backend/AddDatasets/addNewDataset.ttl','w') as file:
-			file.write("PREFIX dct: <http://purl.org/dc/terms/>")
-			file.write("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>")
-			file.write("PREFIX owl: <http://www.w3.org/2002/07/owl#>")
-			file.write("PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>")
-			file.write("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>")
-			file.write("PREFIX foaf: <http://xmlns.com/foaf/0.1/>")
-			file.write("PREFIX disco: <http://rdf-vocabulary.ddialliance.org/discovery>")
-			file.write("PREFIX dcat: <https://www.w3.org/TR/vocab-dcat/>")
-			file.write("PREFIX bdo: <http://bigdataocean.eu/bdo/>")
-			file.write("PREFIX ids: <http://industrialdataspace/information-model/>")
-			file.write("PREFIX qudt: <http://qudt.org/schema/qudt/>")
-			file.write("PREFIX unit: <http://qudt.org/vocab/unit/>")
-			file.write("\n")
-			file.write("INSERT DATA {\n")
-			file.write("	bdo:"+request.form["identifier"]+" a dcat:Dataset ;\n")
-			file.write("		dct:identifier \""+request.form['identifier']+"\" ;\n")
-			file.write("		dct:title \""+request.form['title']+"\" ;\n")
-			file.write("		dct:description \""+request.form['description']+"\" ;\n")
-			file.write("		dcat:subject <"+request.form['tokenfield_subject']+"> ;\n")
-			file.write("		dcat:theme <"+request.form['tokenfield_keywords']+"> ;\n")
-			file.write("		dct:conformsTo \""+request.form['standards']+"\" ;\n")
-			file.write("		dct:format \""+request.form['format']+"\" ;\n")
-			file.write("		dct:language \""+request.form['tokenfield_language']+"\" ;\n")
-			file.write("		foaf:homepage \""+request.form['homepage']+"\" ;\n")
-			file.write("		dct:publisher \""+request.form['publisher']+"\" ;\n")
-			file.write("		dct:accessRights \""+request.form['access_rights']+"\" ;\n")
-			file.write("		dct:issued \""++"\"^^xsd:dateTime ;\n")
-			file.write("		dct:modified \""++"\"^^xsd:dateTime ;\n")
-			file.write("		dct:spatial "+request.form['geo_location']+" ;\n")
-			file.write("		dct:conformsTo \""+request.form['coordinate_sys']+"\" ;\n")
-			file.write("		bdo:verticalCoverage bdo:"+request.form['identifier']+"_VC ;\n")
-			file.write("		bdo:verticalLevel \""+request.form['vertical_level']+"\";\n")
-			file.write("		bdo:timeCoverage \""+request.form['temp_coverage']+"\" ;\n")
-			file.write("		bdo:temporalResolution \""+request.form['temp_resolution']+"\" ;\n")
-			#.file.write("		disco:variable .")
-			file.write("}")
-			file.close()
+		uri = "<http://bigdataocean.eu/bdo/"+request.form['identifier']+">\n"
+		print (uri)
+		with open(globalPath+'/Backend/AddDatasets/addNewDataset.ttl','w') as file:
+		 	file.write("PREFIX dct: <http://purl.org/dc/terms/>\n")
+		 	file.write("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n")
+		 	file.write("PREFIX owl: <http://www.w3.org/2002/07/owl#>\n")
+		 	file.write("PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n")
+		 	file.write("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n")
+		 	file.write("PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n")
+		 	file.write("PREFIX disco: <http://rdf-vocabulary.ddialliance.org/discovery>\n")
+		 	file.write("PREFIX dcat: <https://www.w3.org/TR/vocab-dcat/>\n")
+		 	file.write("PREFIX bdo: <http://bigdataocean.eu/bdo/>\n")
+		 	file.write("PREFIX ids: <http://industrialdataspace/information-model/>\n")
+		 	file.write("PREFIX qudt: <http://qudt.org/schema/qudt/>\n")
+		 	file.write("PREFIX unit: <http://qudt.org/vocab/unit/>\n")
+		 	file.write("\n")
+		 	file.write("INSERT DATA {\n")
+		 	file.write("bdo:"+request.form["identifier"]+" a dcat:Dataset ;\n")
+		 	file.write("dct:identifier \""+request.form['identifier']+"\" ;\n")
+		 	file.write("dct:title \""+request.form['title']+"\" ;\n")
+		 	file.write("dct:description \""+request.form['description']+"\" ;\n")
+		 	file.write("dcat:subject <"+request.form['tokenfield_subject']+"> ;\n")
+		 	file.write("dcat:theme <"+request.form['tokenfield_keywords']+"> ;\n")
+		 	file.write("dct:conformsTo \""+request.form['standards']+"\" ;\n")
+		 	file.write("dct:format \""+request.form['tokenfield_format']+"\" ;\n")
+		 	file.write("dct:language \""+request.form['tokenfield_language']+"\" ;\n")
+		 	file.write("foaf:homepage \""+request.form['homepage']+"\" ;\n")
+		 	file.write("dct:publisher \""+request.form['publisher']+"\" ;\n")
+		 	file.write("dct:accessRights \""+request.form['access_rights']+"\" ;\n")
+		 	file.write("dct:issued \""+request.form['issued_date']+"\"^^xsd:dateTime ;\n")
+		 	file.write("dct:modified \""+request.form['modified_date']+"\"^^xsd:dateTime ;\n")
+		 	file.write("dct:spatial \""+request.form['tokenfield_geo_loc']+"\" ;\n")
+		 	file.write("dct:conformsTo \""+request.form['coordinate_sys']+"\" ;\n")
+		 	file.write("bdo:verticalCoverage bdo:"+request.form['identifier']+"_VC ;\n")
+		 	file.write("bdo:verticalLevel \""+request.form['vertical_level']+"\";\n")
+		 	file.write("bdo:timeCoverage [ids:beginning \""+request.form['temp_coverage_begin']+"\"^^xsd:dateTime ;\n")
+		 	file.write("ids:end \""+request.form['temp_coverage_end']+"\"^^xsd:dateTime] ;\n")
+		 	file.write("bdo:temporalResolution \""+request.form['time_reso']+"\" .\n")
+		 	#.file.write("		disco:variable .")
+		 	file.write("}")
+		 	file.close()
 		path2TTL = globalPath + "/Backend/AddDatasets/addNewDataset.ttl"
 		command = globalPath + '/Backend/bdodatasets/target/BDODatasets-bdodatasets/BDODatasets/bin/addDataset2bdo "%s" "%s"' %(uri, path2TTL)
 		try:
 			process = subprocess.check_output([command], shell="True")
 		except subprocess.CalledProcessError as e:
 			return render_template('500.html')
-		if process == "Successful":
+		if process == "Successful\n":
+			print (process)
 			return render_template('index.html')
 		else:
+			print (process)
 			return render_template('500.html')
 
 @app.route('/edit', methods=['GET', 'POST'])
