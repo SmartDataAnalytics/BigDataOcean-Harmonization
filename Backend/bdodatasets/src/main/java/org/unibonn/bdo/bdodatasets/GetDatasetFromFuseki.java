3aa2027baa39df11dbbe06afa12f898f3c1d5421
Ana Trillos <anatrillosu@gmail.com>
2017-11-16 15:58:16 +0100

    get dataset

--- Backend/bdodatasets/src/main/java/org/unibonn/bdo/bdodatasets/GetDatasetFromFuseki.java
+++ Backend/bdodatasets/src/main/java/org/unibonn/bdo/bdodatasets/GetDatasetFromFuseki.java
@@ -7,6 +7,7 @@ import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 
 /**
@@ -30,12 +31,21 @@ public class GetDatasetFromFuseki {
 
 	public static void exec(String Uri) {
 		
-		String[][] result = new String[30][2];
 		Dataset dataset = new Dataset();
+		int i = 0;
+		QueryExecution qc = QueryExecutionFactory.sparqlService(
+				"http://localhost:3030/bdoHarmonization/query","SELECT (count(distinct ?p) as ?prop) WHERE{"+Uri+" ?p ?o}");
+		ResultSet count = qc.execSelect();
+		QuerySolution countSol = count.nextSolution();
+		System.out.println(countSol);		
+		int num = Integer.parseInt(countSol.toString().replaceAll("\\D+",""));
+		
+		String[][] result = new String[num][2];
 		QueryExecution qe = QueryExecutionFactory.sparqlService(
 					"http://localhost:3030/bdoHarmonization/query","SELECT ?p ?o WHERE{"+Uri+" ?p ?o}");
 		ResultSet results = qe.execSelect();
-		int i = 0;
+		ResultSetFormatter.out(System.out, results);
+		
 		while(results.hasNext()){
 			QuerySolution solution = results.nextSolution();
 			RDFNode node = solution.get("p");
@@ -51,6 +61,32 @@ public class GetDatasetFromFuseki {
 				dataset.setIdentifier(result[j][1]);
 			}else if(result[j][0].matches("http://purl.org/dc/terms/title")){
 				dataset.setTitle(result[j][1]);
+			}else if(result[j][0].matches("http://purl.org/dc/terms/description")){
+				dataset.setDescription(result[j][1]);
+			}else if(result[j][0].matches("https://www.w3.org/TR/vocab-dcat/subject")){
+				dataset.setSubject(result[j][1]);
+			}else if(result[j][0].matches("https://www.w3.org/TR/vocab-dcat/theme")){
+				dataset.setKeywords(result[j][1]);
+			}else if(result[j][0].matches("http://purl.org/dc/terms/conformsTo")){
+				dataset.setStandards(result[j][1]);
+			}else if(result[j][0].matches("http://purl.org/dc/terms/format")){
+				dataset.setFormat(result[j][1]);
+			}else if(result[j][0].matches("http://purl.org/dc/terms/language")){
+				dataset.setLanguage(result[j][1]);
+			}else if(result[j][0].matches("http://xmlns.com/foaf/0.1/homepage")){
+				dataset.setHomepage(result[j][1]);
+			}else if(result[j][0].matches("http://purl.org/dc/terms/publisher")){
+				dataset.setPublisher(result[j][1]);
+			}else if(result[j][0].matches("http://purl.org/dc/terms/accessRights")){
+				dataset.setAccessRights(result[j][1]);
+			}else if(result[j][0].matches("http://purl.org/dc/terms/issued")){
+				dataset.setIssuedDate(result[j][1]);
+			}else if(result[j][0].matches("http://purl.org/dc/terms/modified")){
+				dataset.setModifiedDate(result[j][1]);
+			}else if(result[j][0].matches("http://purl.org/dc/terms/spatial")){
+				dataset.setGeoLocation(result[j][1]);
+			}else if(result[j][0].matches("<http://bigdataocean.eu/bdo/temporalResolution")){
+				dataset.setTimeResolution(result[j][1]);
 			}
 		}
 		
