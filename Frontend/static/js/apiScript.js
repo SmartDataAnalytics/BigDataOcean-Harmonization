1f25c0970f17dabaf798b8d2e0fee3069747f440
Jaime M Trillos <jaimemanueltrillos@gmail.com>
2018-01-22 17:03:30 +0100

    new design for apis

--- Frontend/static/js/apiScript.js
+++ Frontend/static/js/apiScript.js
@@ -1,5 +1,16 @@
+//activate accordion jquery plugin
 $( function() {
     $( "#accordion" ).accordion({
+      collapsible: true,
+      autoHeight: false,
+      active: false,
+    });
+    $( "#accordion1" ).accordion({
+      collapsible: true,
+      autoHeight: false,
+      active: false,
+    });
+    $( "#accordion2" ).accordion({
       collapsible: true,
       autoHeight: false,
       active: false,
@@ -34,6 +45,113 @@ jQuery(document).ready(function($) {
       if (token.value === event.attrs.value)
         event.preventDefault();
     });
+  });
+
+  var resourceSubjects = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: 'static/json/subject.json'
+  });
+  resourceSubjects.initialize(); 
+
+  var resourceKeywords = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: 'static/json/keywords.json'
+  });
+  resourceKeywords.initialize();
+
+  var resourceGeoLoc = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: 'static/json/marineregions.json'
+  });
+  resourceGeoLoc.initialize();
+
+  $('#tokenfield_subject').tokenfield({
+    typeahead: [null, {
+      source: resourceSubjects.ttAdapter(),
+      displayKey: 'text'
+    }],
+    showAutocompleteOnFocus: true
+  }); 
+
+  //if the token is duplicated then it is not created
+  $('#tokenfield_subject').on('tokenfield:createtoken', function (event) {
+    var existingTokens = $(this).tokenfield('getTokens');
+    $.each(existingTokens, function(index, token) {
+      if (token.value === event.attrs.value)
+        event.preventDefault();
+    });
+  });
+
+  //if the token has value with http then it is created
+  $('#tokenfield_subject').on('tokenfield:createtoken', function (event) {
+    var flag = false;
+    var pattern = /http/;
+    $.each(resourceSubjects, function(index, value) {
+      var exists = pattern.test(event.attrs.value);
+      flag = exists;
+    });
+    if(!flag) {
+      event.preventDefault(); //prevents creation of token
+    }
+  });
+
+  $('#tokenfield_keywords').tokenfield({
+    typeahead: [null, {
+      source: resourceKeywords.ttAdapter(),
+      displayKey: 'text'
+    }],
+    showAutocompleteOnFocus: true
+  });
+
+  $('#tokenfield_keywords').on('tokenfield:createtoken', function (event) {
+    var existingTokens = $(this).tokenfield('getTokens');
+    $.each(existingTokens, function(index, token) {
+      if (token.value === event.attrs.value)
+        event.preventDefault();
+    });
+  });
+
+  $('#tokenfield_keywords').on('tokenfield:createtoken', function (event) {
+    var flag = false;
+    var pattern = /http/;
+    $.each(resourceKeywords, function(index, value) {
+      var exists = pattern.test(event.attrs.value);
+      flag = exists;
+    });
+    if(!flag) {
+      event.preventDefault(); //prevents creation of token
+    }
+  });
+
+  $('#tokenfield_geo_loc').tokenfield({
+    typeahead: [null, {
+      source: resourceGeoLoc.ttAdapter(),
+      displayKey: 'text'
+    }],
+    showAutocompleteOnFocus: true
+  });
+
+  $('#tokenfield_geo_loc').on('tokenfield:createtoken', function (event) {
+    var existingTokens = $(this).tokenfield('getTokens');
+    $.each(existingTokens, function(index, token) {
+      if (token.value === event.attrs.value)
+        event.preventDefault();
+    });
+  });  
+
+  $('#tokenfield_geo_loc').on('tokenfield:createtoken', function (event) {
+    var flag = false;
+    var pattern = /http/;
+    $.each(resourceGeoLoc, function(index, value) {
+      var exists = pattern.test(event.attrs.value);
+      flag = exists;
+    });
+    if(!flag) {
+      event.preventDefault(); //prevents creation of token
+    }
   });
 
 });
